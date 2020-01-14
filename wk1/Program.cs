550307b6db59021209ee2f1b3e69985906dafb7c
Geoffrey <rog13007@gmail.com>
2020-01-14 13:40:45 -0700

    Add project files.

---
+++ wk1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace wk1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            string myName;
+            Console.Write("Enter your name: ");
+            myName = Console.ReadLine();
+
+            
+            string loc;
+            Console.Write("Enter your location: ");
+            loc = Console.ReadLine();
+            
+
+            Console.WriteLine($"My name is: {myName}");
+            //Console.WriteLine($"I am {0} at {1}", myName, loc);
+            */
+
+            var currDate = DateTime.Now;
+            Console.WriteLine("The current date is: " + currDate.ToString("d"));
+
+            var christmas = new DateTime(2020, 12, 25);
+            var daysToXmas = christmas - currDate;
+            Console.WriteLine("Days until Christmas: " + daysToXmas.ToString());
+
+
+            Console.WriteLine("Press any key to cotinue");
+            Console.ReadKey();
+        }
+    }
+}
